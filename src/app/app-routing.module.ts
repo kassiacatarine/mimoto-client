a41342a796c51851190d4a4c40cfc280b1e80085
Kassia Catarine <kassia_catarine.15@hotmail.com>
2019-04-30 16:35:21 -0300

    Alterando tamanho do avatar e mudando o a chamada do layout na rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutMainComponent } from './shared/components/layouts/layout-main/layout-main.component';
 
 const routes: Routes = [
+  { path: '', component: LayoutMainComponent, pathMatch: 'full',  children: [
+    {
+      path: 'users', loadChildren: '../app/modules/users/users.module#UsersModule'
+    }
+  ]},
   {
     path: '', loadChildren: '../app/modules/auth/auth.module#AuthModule',
-  },
-  {
-    path: 'users', loadChildren: '../app/modules/users/users.module#UsersModule'
   }
 ];
 
--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LayoutMainComponent } from 'src/app/shared/components/layouts/layout-main/layout-main.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutMainComponent, pathMatch: 'full',  children: [
-    { path: '', component: UsersListComponent }
-  ]}
+  { path: '', component: UsersListComponent }
 ];
 
 @NgModule({
