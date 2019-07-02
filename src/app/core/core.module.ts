ff94230b62798aa71b8c2ad0473b422587a6a9e1
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-07-02 18:52:17 -0300

    Adicionanando dialog de confirm

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ListAvatarComponent } from './components/lists/list-avatar/list-avatar.component';
 import { ListLinkComponent } from './components/lists/list-link/list-link.component';
+import { ConfirmDialogComponent } from './components/dialog/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
     ListAvatarComponent,
-    ListLinkComponent
+    ListLinkComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     CommonModule,
